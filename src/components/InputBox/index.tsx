61c28daad223245d12957aa2b4b779dfa2c20e8d
devJiraynor <jiraynor@naver.com>
2024-08-26 10:51:58 +0900

    2024.08.26. 10:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './style.css';
 
 interface Props {
@@ -7,20 +7,27 @@ interface Props {
     placeholder: string;
     value: string;
     buttonName?: string;
+
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onButtonClick?: () => void;
 }
 
-export default function InputBox({ label, type, placeholder, value, buttonName }: Props) {
-
-    // 이름
-    // 아이디
-    // 비밀번호
+export default function InputBox({ 
+    label, 
+    type, 
+    placeholder, 
+    value, 
+    buttonName, 
+    onChange,
+    onButtonClick,
+}: Props) {
 
     return (
         <div className="input-box">
             <div className="label">{label}</div>
             <div className="input-area">
-                <input value={value} type={type} placeholder={placeholder} />
-                {buttonName && <div className="input-button disable">{buttonName}</div>}
+                <input value={value} type={type} placeholder={placeholder} onChange={onChange} />
+                {buttonName && <div className={`input-button ${value ? 'active' : 'disable'}`} onClick={onButtonClick}>{buttonName}</div>}
             </div>
             <div className="message"></div>
         </div>
--- src/views/Auth/index.tsx
+++ src/views/Auth/index.tsx
@@ -15,6 +15,46 @@ export default function Auth() {
         const { value } = event.target;
         setName(value);
     };
+
+    const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setId(value);
+    };
+
+    const onPasswordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setPassword(value);
+    };
+
+    const onPasswordCheckChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setPasswordCheck(value);
+    };
+
+    const onTelNumberChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setTelNumber(value);
+    };
+
+    const onAuthNumberChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setAuthNumber(value);
+    };
+
+    const onIdCheckClickHandler = () => {
+        if (!id) return;
+        alert('중복 확인 버튼 클릭!');
+    };
+
+    const onTelNumberSendClickHandler = () => {
+        if (!telNumber) return;
+        alert('인증번호 전송!');
+    };
+
+    const onAuthNumberCheckClickHandler = () => {
+        if (!authNumber) return;
+        alert('인증번호 확인!');
+    };
     
     return (
         <div id="auth-wrapper">
@@ -35,12 +75,12 @@ export default function Auth() {
                     <div style={{ width: '64px' }} className="divider"></div>
 
                     <div className="input-container">
-                        <InputBox value={name} label='이름' type='text' placeholder='이름을 입력해주세요.' />
-                        <InputBox value={id} label='아이디' type='text' placeholder='아이디를 입력해주세요.' buttonName='중복 확인' />
-                        <InputBox value={password} label='비밀번호' type='password' placeholder='비밀번호를 입력해주세요.' />
-                        <InputBox value={passwordCheck} label='비밀번호 확인' type='password' placeholder='비밀번호를 입력해주세요.' />
-                        <InputBox value={telNumber} label='전화번호' type='text' placeholder='-빼고 입력해주세요.' buttonName='전화번호 인증' />
-                        <InputBox value={authNumber} label='인증번호' type='text' placeholder='인증번호 4자리를 입력해주세요.' buttonName='인증 확인' />
+                        <InputBox value={name} label='이름' type='text' placeholder='이름을 입력해주세요.' onChange={onNameChangeHandler} />
+                        <InputBox value={id} label='아이디' type='text' placeholder='아이디를 입력해주세요.' buttonName='중복 확인' onChange={onIdChangeHandler} onButtonClick={onIdCheckClickHandler} />
+                        <InputBox value={password} label='비밀번호' type='password' placeholder='비밀번호를 입력해주세요.' onChange={onPasswordChangeHandler} />
+                        <InputBox value={passwordCheck} label='비밀번호 확인' type='password' placeholder='비밀번호를 입력해주세요.' onChange={onPasswordCheckChangeHandler} />
+                        <InputBox value={telNumber} label='전화번호' type='text' placeholder='-빼고 입력해주세요.' buttonName='전화번호 인증' onChange={onTelNumberChangeHandler} onButtonClick={onTelNumberSendClickHandler} />
+                        <InputBox value={authNumber} label='인증번호' type='text' placeholder='인증번호 4자리를 입력해주세요.' buttonName='인증 확인' onChange={onAuthNumberChangeHandler} onButtonClick={onAuthNumberCheckClickHandler} />
                     </div>
 
                     <div className="button-container">
