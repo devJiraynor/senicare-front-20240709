c6bf5f1c78af0afa528a4064276712bcc787adef
devJiraynor <jiraynor@naver.com>
2024-09-05 09:53:03 +0900

    2024.09.05. 09:50

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,16 +1,47 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Outlet } from 'react-router';
+import './style.css';
+
+// component: 로고 컴포넌트 //
+function Logo() {
+
+    // render: 로고 컴포넌트 렌더링 //
+    return (
+        <div id='layout-log'></div>
+    );
+
+}
+
+// component: 상단 컴포넌트 //
+function Top() {
+
+    // render: 상단 컴포넌트 //
+    return (
+        <div id='layout-top'></div>
+    );
+
+}
+
+// component: 좌측 네비게이션 컴포넌트 //
+function SideNavigation() {
+
+    // render: 좌측 네비게이션 컴포넌트 //
+    return (
+        <div id='layout-side-navigation'></div>
+    );
+
+}
 
 // component: 메인 레이아웃 컴포넌트 //
 export default function MainLayout() {
 
     // render: 메인 레이아웃 컴포넌트 렌더링 //
     return (
-        <div>
-            <div>로고</div>
-            <div>상단</div>
-            <div>좌측 네비게이션</div>
+        <div id='main-layout'>
+            <Logo />
+            <Top />
+            <SideNavigation />
             <Outlet />
         </div>
-    )
+    );
 }
