30f71cea6ba77c575b44a8b496995e4c1acc7dac
devJiraynor <jiraynor@naver.com>
2024-09-12 17:32:58 +0900

    2024.09.12. 17:50

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,33 +1,43 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { IdCheckRequestDto, TelAuthRequestDto } from "./dto/request/auth";
 import { ResponseDto } from "./dto/response";
 
+// variable: API URL 상수 //
+const SENICARE_API_DOMAIN = 'http://localhost:4000';
+
+const AUTH_MODULE_URL = `${SENICARE_API_DOMAIN}/api/v1/auth`;
+
+const ID_CHECK_API_URL = `${AUTH_MODULE_URL}/id-check`;
+const TEL_AUTH_API_URL = `${AUTH_MODULE_URL}/tel-auth`;
+const TEL_AUTH_CHECK_API_URL = `${AUTH_MODULE_URL}/tel-auth-check`;
+const SIGN_UP_API_URL = `${AUTH_MODULE_URL}/sign-up`;
+const SIGN_IN_API_URL = `${AUTH_MODULE_URL}/sign-in`;
+
+// function: response data 처리 함수 //
+const responseDataHandler = <T>(response: AxiosResponse<T, any>) => {
+    const { data } = response;
+    return data;
+};
+
+// function: response error 처리 함수 //
+const responseErrorHandler = (error: any) => {
+    if (!error.response) return null;
+    const { data } = error.response;
+    return data as ResponseDto;
+};
+
 // function: id check api 요청 함수 //
 export const idCheckRequest = async (requestBody: IdCheckRequestDto) => {
-    const responseBody = await axios.post("http://localhost:4000/api/v1/auth/id-check", requestBody)
-        .then(response => {
-            const { data } = response;
-            return data as ResponseDto;
-        })
-        .catch(error => {
-            if (!error.response) return null;
-            const { data } = error.response;
-            return data as ResponseDto;
-        });
+    const responseBody = await axios.post(ID_CHECK_API_URL, requestBody)
+        .then(responseDataHandler<ResponseDto>)
+        .catch(responseErrorHandler);
     return responseBody;
 };
 
 // function: tel auth api 요청 함수 //
 export const telAuthRequest = async (requestBody: TelAuthRequestDto) => {
-    const responseBody = await axios.post("http://localhost:4000/api/v1/auth/tel-auth", requestBody)
-        .then(response => {
-            const { data } = response;
-            return data as ResponseDto;
-        })
-        .catch(error => {
-            if (!error.response) return null;
-            const { data } = error.response;
-            return data as ResponseDto;
-        });
+    const responseBody = await axios.post(TEL_AUTH_API_URL, requestBody)
+        .then(responseDataHandler<ResponseDto>)
+        .catch(responseErrorHandler);
     return responseBody;
 };
